e3e50dd5eb9a5c3c5c1b57df20e4a629d8d047ad
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-04-23 20:53:45 +0330

    Add DislikePostProvider to handle post dislike functionality

---
+++ lib/app/modules/home/providers/dislike_post_provider.dart
@@ -0,0 +1,31 @@
+import 'package:fullstack_socialmedia/app/data/Constants/constants.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class DislikePostProvider extends GetConnect {
+  Future dislikePost(int likeId) async {
+    try {
+      final response = await delete(
+        '${Constants.baseUrl}/api/likes/delete/$likeId',
+        headers: {'auth': GetStorage().read(Constants.tokenKey)},
+      );
+      if (response.body != null) {
+        if (response.status.isOk) {
+          return response.body;
+        } else if (response.status.isServerError) {
+          throw 'Server is not reachable\nPlease try again later';
+        } else if (response.status.isNotFound) {
+          throw 'Not Found!';
+        } else if (response.status.isForbidden) {
+          throw 'Bad request\nCheck data and try again...';
+        } else {
+          throw response.body['message'] ?? response.bodyString;
+        }
+      } else {
+        throw 'Something went wrong!\nPlease try again later';
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
